1d3dea7162205ff9bff937fe0bfc02fca5fdae9b
alexander <svsanek@gmail.com>
2022-09-06 18:58:57 +0200

    add some log in MainActivity

--- app/src/main/java/com/pronin/androidbegginers/MainActivity.java
+++ app/src/main/java/com/pronin/androidbegginers/MainActivity.java
@@ -3,12 +3,15 @@ package com.pronin.androidbegginers;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate() called with: savedInstanceState = [" + savedInstanceState + "]");
         setContentView(R.layout.activity_main);
     }
 }
